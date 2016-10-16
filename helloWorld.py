70e87dd4d79f92deab89d7acdcbaf73576a43de8
sandeep <sandeepMacbookPro@kandekarmac.attlocal.net>
2016-10-15 22:28:44 -0500

    Completed | added a systemout file call

---
+++ helloWorld.py
@@ -0,0 +1,13 @@
+my_name = 'Aadya'
+print " "
+print "********* output *************"
+print " "
+print " "
+print "i'm Svara's little sister, the cutest girl in the whole wide world -  %s." % my_name
+print " "
+print " "
+print "********* end ****************"
+print " "
+## Run it as
+## python helloWorld.py
+## you should see the output as print
